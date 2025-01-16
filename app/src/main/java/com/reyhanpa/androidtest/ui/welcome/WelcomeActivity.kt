497f1a19d4e21c7dfaac3b16429d12afe964254f
ReyhanPA <reyhanputra95@gmail.com>
2025-01-16 23:12:42 +0700

    feat: add logic for first screen

--- app/src/main/java/com/reyhanpa/androidtest/ui/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/reyhanpa/androidtest/ui/welcome/WelcomeActivity.kt
@@ -2,14 +2,17 @@ package com.reyhanpa.androidtest.ui.welcome
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.reyhanpa.androidtest.databinding.ActivityMainBinding
+import com.reyhanpa.androidtest.R
+import com.reyhanpa.androidtest.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityWelcomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportActionBar?.title = getString(R.string.title_second_screen)
     }
 }
